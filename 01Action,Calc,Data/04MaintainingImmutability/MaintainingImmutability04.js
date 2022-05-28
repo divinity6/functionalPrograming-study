5c202157f27b0cd5c3fc25d3ef8ac58021c358fd
divinity6 <divinity666@naver.com>
2022-05-28 18:30:45 +0900

    방어적 복사

--- 01Action,Calc,Data/04MaintainingImmutability/MaintainingImmutability04.js
+++ 01Action,Calc,Data/04MaintainingImmutability/MaintainingImmutability04.js
@@ -26,11 +26,39 @@
  *      4. 함수형 프로그래밍 언어에는 빠른 구현체가 있다
  *      --> 데이터 구조를 복사할때 최대한 많은 구조를 공유
  *          ( 더 적은 메모리를 사용하고 가비지 콜렉터의 부담을 줄여줌 )
- *
- *       134p
  */
 
-title( '전역변수' );
-{
+/**
+ *           ===== 객체에 대한 카피-온-라이트 =====
+ *
+ *      - 깊은 복사가 아닌 얕은 복사를 사용하면 최상위 데이터만 복사함
+ *
+ *      - 구조적 공유( structural sharing )이라고도 하는데,
+ *        따라서 메모리를 적게 사용하고, 깊은 복사보다 빠르다
+ *
+ *      --> 얕은 복사를 사용해야하는 이유가 있엇네
+ *
+ *      --> 그런데 또 읽으니 중첩된 모든 데이터 구조가 바뀌지 않아야 한다고 하네...
+ *
+ */
 
+title( '불변성으로 객체 맨들기' );
+{
+    function objectSet( object , key , value ){
+        return Object.assign( {} , object )[ key ] = value;
+    }
+
+    function setPrice( item , new_price ){
+        return objectSet( item , "price" , new_price );
+    }
+
+    function setQuantity( item , new_quantity ){
+        return objectSet( item , "quantity" , new_quantity );
+    }
+
+    function objectDelete( object , key ){
+        var _object = Object.assign( {} , object );
+        delete _object[ key ];
+        return _object;
+    }
 }
